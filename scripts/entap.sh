70b43bbfc1f3d74ddef422896ae8e5d01aa381b5
Cynthia Webster <cynthia.webster@uconn.edu>
2024-10-25 10:38:08 -0400

    add entap

--- scripts/entap.sh
+++ scripts/entap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run -it \
-  -v $(pwd):/data \
+  -v /workspace/bgaworkshop-2024/scripts:/workspace/bgaworkshop-2024/scripts \
   plantgenomics/entap:latest \
-  /bin/bash -c "EnTAP --runP --run-ini entap_run.params --entap-ini entap_config.ini"
+  /bin/bash -c "EnTAP --runP --run-ini /workspace/bgaworkshop-2024/scripts/entap_run.params --entap-ini /workspace/bgaworkshop-2024/scripts/entap_config.ini"
